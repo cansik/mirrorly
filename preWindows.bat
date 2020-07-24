76487cfdf2c98319e4d6c4d52b749bc0a471e0c6
Florian Bruggisser <florian@bildspur.ch>
2020-07-24 16:26:02 +0200

    added wget to windows

--- preWindows.bat
+++ preWindows.bat
@@ -1,11 +1,16 @@
 echo "installing all dependencies for mirrorly, this can take a while!"
 
+REM install wget
+choco install wget
+
 REM install gstreamer
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://gstreamer.freedesktop.org/data/pkg/windows/1.16.2/gstreamer-1.0-msvc-x86_64-1.16.2.msi" "gstreamer.msi"
-msiexec /i gstreamer.msi /quiet /qn /norestart
+wget "https://gstreamer.freedesktop.org/data/pkg/windows/1.16.2/gstreamer-1.0-msvc-x86_64-1.16.2.msi" -O "gstreamer.msi"
+dir
+msiexec /passive INSTALLDIR=gstreamer /i gstreamer.msi
+dir
 
-REM load gstreamer
-
+REM needs chcholately installed
+refreshenv
 
 REM download elements
 git clone --recurse-submodules  https://github.com/cycfi/elements.git
