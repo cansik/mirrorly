d3ac9f97fcba78734e199e2e15097afa8ad4aab6
Florian Bruggisser <florian@bildspur.ch>
2020-07-24 16:49:18 +0200

    added msbuild tool so msbuild is installed

--- build.bat
+++ build.bat
@@ -2,5 +2,7 @@ echo "building..."
 mkdir build
 cd build
 cmake -G "Visual Studio 15 2017" ..
+
 dir
+
 msbuild
--- preWindows.bat
+++ preWindows.bat
@@ -1,13 +1,16 @@
 echo "installing all dependencies for mirrorly, this can take a while!"
 
-REM install wget
+REM install wget and pkconfig
 choco install wget
+choco install pkgconfiglite
+choco install microsoft-build-tools
 
 REM install gstreamer
 wget -nv "https://gstreamer.freedesktop.org/data/pkg/windows/1.16.2/gstreamer-1.0-msvc-x86_64-1.16.2.msi" -O "gstreamer.msi"
-dir
-msiexec /passive INSTALLDIR=gstreamer /i gstreamer.msi
-dir
+msiexec /passive INSTALLDIR=gstreamer /i gstreamer.msi /L*V! "gstreamer.log"
+
+echo "install log:"
+type gstreamer.log
 
 REM needs chcholately installed
 refreshenv
