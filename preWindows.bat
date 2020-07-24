e70e0c4ad3bdc709436204eb87feea08760a0317
Florian Bruggisser <florian@bildspur.ch>
2020-07-24 12:19:48 +0200

    added gstreamer to windows pre

--- preWindows.bat
+++ preWindows.bat
@@ -1,6 +1,10 @@
 echo "installing all dependencies for mirrorly, this can take a while!"
 
-# download elements
+REM install gstreamer
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://gstreamer.freedesktop.org/data/pkg/windows/1.16.2/gstreamer-1.0-msvc-x86_64-1.16.2.msi" "gstreamer.msi"
+msiexec /i gstreamer.msi /quiet /qn /norestart
+
+REM download elements
 git clone --recurse-submodules  https://github.com/cycfi/elements.git
 
 echo "all done!"
